91b424248059ce2f1c6b50cfc5300a9c67eb21ff
William Chan <wchan1992@gmail.com>
2019-05-12 17:18:56 -0700

    Add styling

--- client/src/components/RepoListItem.jsx
+++ client/src/components/RepoListItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const RepoListItem = (props) => (
-  <div>
+  <div className="RepoListItem">
     <h2>Username: {props.repo.username}</h2>
-    <img src={props.repo.avatar_url} />
+    <img className="UserPicture" src={props.repo.avatar_url} />
     <div>Repo Name: <a href={props.repo.repo_url}>{props.repo.repo_name}</a></div>
-    <div>Repo URL: {props.repo.repo_url}</div>
+    {/* <div>Repo URL: {props.repo.repo_url}</div> */}
     <div>Forks: {props.repo.forks}</div>
   </div>
 )
